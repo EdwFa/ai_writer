d214ad32a5813d5d30813e224a9544df50dfda05
EdwFa <efart@mail.ru>
2024-06-01 08:01:59 +0300

    update

--- main.py
+++ main.py
@@ -343,9 +343,6 @@ try:
             user_key = st.text_input("Введите, выданный вам ключ ... : 👇", "", type="password")
             if user_key == st.secrets["USER_KEY"]:
                 groq_input_key = st.secrets["API_KEY"]
-            else:
-                st.info('user key - '+user_key, icon="ℹ️")
-                # raise ValueError("")
 
         topic_text = st.text_input("О чем вы хотите написать свою книгу? ", "")
 
