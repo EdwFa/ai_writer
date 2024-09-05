fd605cb96f31052ecdb097c6968f0496b4b27f21
EdwFa <efart@mail.ru>
2024-09-05 20:13:48 +0300

    update

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class AI_Model():
         self.top_P = top_P
 # иницифализация моделей для генерации структуры и контента
 model_struct = AI_Model(name='LLaMA3-8b-8192', version='8B', temperature=0.2, max_tokens=8192, top_P=1.0)
-model_content = AI_Model(name='LLaMA3-70b-8192', version='70B', temperature=0.3, max_tokens=8192, top_P=1.0)
+model_content = AI_Model(name='LLaMA3-70b-8192', version='70B', temperature=0.2, max_tokens=8192, top_P=1.0)
 
 # Обрабатываем статистические параметры генерации 8b ламмой
 class GenerationStatistics:
